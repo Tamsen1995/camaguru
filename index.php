97807becd093d7bc1fe897afc632fb4fc175c1b6
Nguyen Tam Anh Bui <tbui@Nguyens-MacBook-Pro.local>
2017-06-29 23:35:54 +0200

    first

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>Camaguru</title>
+    <style type="text/css">
+
+    </style>
+</head>
+
+    <body>
+        <div id ="pageTop"></div>
+        <div id ="pageMiddle"></div>
+        <div id ="pageButton"></div>
+
+
+
+    </body>
+
+</html>
